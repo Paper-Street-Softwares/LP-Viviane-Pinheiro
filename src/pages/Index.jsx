1814975a0d58fdf292ec7dbc4c29084c6593e0f1
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-08 17:35:10 -0300

    fix: Features

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -65,11 +65,11 @@ export default function Index() {
 
       <Features
         colorMode={colorMode}
-        defaultFeature={false}
+        defaultFeature={true}
         button={false}
         modalWithCards={false}
         paragraphs={false}
-        sixCards={true}
+        sixCards={false}
         paragraphsModal={false}
       />
 
