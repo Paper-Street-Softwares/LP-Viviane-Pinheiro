f8c41b97185f6eb1ea8cb8169c64ed43553e81b6
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-08 17:28:00 -0300

    fix: Infos content e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -50,14 +50,14 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dra. Viviane Pinheiro",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
   domain: "vivianepinheiro.adv.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "18",
+    firstPart: "99733", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "5295", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -93,7 +93,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "A_definir", // Sem o @
+  instagramProfile: "vivi.pinheirol", // Sem o @
   facebookProfile: "A_Definir",
   linkeDinProfile: "A_Definir",
   x: "A_Definir",
