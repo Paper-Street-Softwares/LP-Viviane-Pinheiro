1247c57e404f7da9e182c6bffbda3851c00e88b8
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-08 17:45:55 -0300

    fix: Blog

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -547,7 +547,7 @@ const content = {
       img: imgSteps,
       labelCards: "Ver mais",
       label: "Clique aqui para ver todas as matérias",
-      blogLink: "A_Definir", //sem "https://"
+      blogLink: "vivianepinheiroadv.wordpress.com", //sem "https://"
     },
     faq: {
       miniTag: "TIRE SUAS DÚVIDAS",
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -77,7 +77,7 @@ export default function Index() {
       <AboutInstagram colorMode={colorMode} socialPrint={true} />
       <CtaSecondary />
       <Steps colorMode={colorMode} />
-      {/* <BlogPosts /> */}
+      <BlogPosts />
       <Faq colorMode={colorMode} />
       {/* <Maps colorMode={colorMode} /> */}
       <FooterSocial colorMode={colorMode} addres={true} obs={false} />
