3231cb401e7536c16f584893b7b6215418986074
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-17 08:08:39 -0300

    fix: Icones da empresa

--- src/components/interactives/Cartao/CartaoRedeSocial.jsx
+++ src/components/interactives/Cartao/CartaoRedeSocial.jsx
@@ -373,7 +373,7 @@ function CartaoRedeSocial({ tipo = "contato", socio }) {
               aria-label={`Link para ${labels[key] || key}`}
             >
               <IconButtonCartao
-                label={labels[key]} // sempre mostra o nome completo da rede
+                label={tipo === "empresa" ? "" : labels[key]} // 🔹 mostra texto apenas se não for "empresa"
                 ariaLabel={`Botão para ${labels[key]}`}
                 icon={svg}
                 width={tipo === "empresa" ? "w-auto" : "min-w-[200px] m-auto"}
